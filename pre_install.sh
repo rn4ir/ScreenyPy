2df8d269a222be04ec97f1a272fa0c80cb377a8b
rn4ir <rn@rahul-nair.com>
2017-10-05 23:11:20 -0400

    preinstall script - extract and echo msg

--- pre_install.sh
+++ pre_install.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-create_dirs(){
+prerequisites(){
     mkdir -p sources/{firefox,driver}
+    sudo yum install gtk3*
 }
 
 download_firefox(){
     curl -o ./sources/firefox/firefox.tar.bz2 https://download-installer.cdn.mozilla.net/pub/firefox/nightly/latest-mozilla-central/firefox-58.0a1.en-US.linux-x86_64.tar.bz2 sources/firefox/firefox.tar.bz2
     tar jxf ./sources/firefox/firefox.tar.bz2 -C ./sources/firefox/
+    version=$(./sources/firefox/firefox/firefox --version)
+    echo -e "Mozilla Firefox download complete.\nVersion: $version"
 }
 
 download_gecko(){
     cd ./sources/driver/; wget https://github.com/mozilla/geckodriver/releases/download/v0.19.0/geckodriver-v0.19.0-linux64.tar.gz; cd -
     tar zxf ./sources/driver/geckodriver-v0.19.0-linux64.tar.gz -C ./sources/driver/
+    version=$(./sources/driver/geckodriver -V | head -1)
+    echo -e "Gecko driver download complete.\nVersion: $version"
 }
 
-
-create_dirs
+prerequisites
 download_firefox
 download_gecko
