306b043d7a2cc32f578db70eaf17bc9890c907cc
rn4ir <rn@rahul-nair.com>
2017-11-15 20:28:25 -0500

    download and extract chromedriver

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PLATFORM=linux64
+VERSION=$(curl http://chromedriver.storage.googleapis.com/LATEST_RELEASE)
+DL_DIR="browsers/chromium-local/chrome-linux/"
+
+sudo yum install GConf2 -y
+
+wget "http://chromedriver.storage.googleapis.com/$VERSION/chromedriver_$PLATFORM.zip"
+unzip chromedriver_$PLATFORM.zip -d $DL_DIR
+rm -fv chromedriver_$PLATFORM.zip
