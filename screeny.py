a13f3f3a6ad8b8684ce36e6a0893dff79ee26f74
rn4ir <rn@rahul-nair.com>
2017-11-15 20:47:05 -0500

    screenypy skel

---
+++ screeny.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from selenium import webdriver
+
+CHROME_BIN = "browsers/chromium-local/chrome-linux/chrome"
+
+options = webdriver.ChromeOptions()
+
+options.binary_location = CHROME_BIN
+options.add_argument('headless')
+options.add_argument('window-size=1200x600')
+options.add_argument('no-sandbox')
+
+driver = webdriver.Chrome(chrome_options=options)
+
+driver.get('https://muchbits.com')
+driver.implicitly_wait(10)
+
+driver.get_screenshot_as_file('muchbits.png')
