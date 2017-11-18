115d88808af47ac42328799b738f1c0134aab538
rn4ir <rn@rahul-nair.com>
2017-11-17 21:09:49 -0500

    chrome binary permissions

--- download_chromium.py
+++ download_chromium.py
@@ -8,7 +8,8 @@ import shutil
 
 DL_DIR = "browsers/chromium-local"
 #https://commondatastorage.googleapis.com/chromium-browser-snapshots/index.html?prefix=Linux_x64/ >> LAST_CHANGE >> chrome-linux.zip
-DL_URL = 'https://storage.googleapis.com/chromium-browser-snapshots/Linux_x64/514946/chrome-linux.zip'
+DL_URL = "https://storage.googleapis.com/chromium-browser-snapshots/Linux_x64/514946/chrome-linux.zip"
+CHROME_PATH = DL_DIR + "/chrome-linux/chrome"
 
 def download_zip(req_url):
     """Download data from url."""
@@ -22,5 +23,7 @@ def download_zip(req_url):
 
     z = zipfile.ZipFile(io.BytesIO(r.content))
     z.extractall(DL_DIR)
+
+    os.chmod(CHROME_PATH, 0o755)
 
 download_zip(DL_URL)
