c88261b15888014c27ab3428ebad1da898401a90
rn4ir <rn@rahul-nair.com>
2017-11-17 21:09:31 -0500

    added libXScrnSaver installation

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ PLATFORM=linux64
 VERSION=$(curl http://chromedriver.storage.googleapis.com/LATEST_RELEASE)
 DL_DIR="browsers/chromium-local/chrome-linux/"
 
-sudo yum install GConf2 -y
+sudo yum install GConf2 libXScrnSaver -y
 
 wget "http://chromedriver.storage.googleapis.com/$VERSION/chromedriver_$PLATFORM.zip"
 sudo unzip chromedriver_$PLATFORM.zip -d $(echo $PATH | cut -d: -f1)
