925c9c6299b68e5399bf89c34867e3b5ec8fe096
rn4ir <rn@rahul-nair.com>
2017-11-17 21:10:11 -0500

    disable-gpu

--- screeny.py
+++ screeny.py
@@ -10,6 +10,7 @@ options.binary_location = CHROME_BIN
 options.add_argument('headless')
 options.add_argument('window-size=1200x600')
 options.add_argument('no-sandbox')
+options.add_argument('disable-gpu')
 
 driver = webdriver.Chrome(chrome_options=options)
 
