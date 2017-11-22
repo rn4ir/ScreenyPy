f6a892181e0c044d155b5d8511b7ea7e34631d48
rn4ir <rn@rahul-nair.com>
2017-11-22 00:16:24 -0500

    proxy

--- screeny.py
+++ screeny.py
@@ -3,6 +3,7 @@
 from selenium import webdriver
 
 CHROME_BIN = "browsers/chromium-local/chrome-linux/chrome"
+PROXY = "149.56.193.80:8080"
 
 options = webdriver.ChromeOptions()
 
@@ -11,10 +12,11 @@ options.add_argument('headless')
 options.add_argument('window-size=1200x600')
 options.add_argument('no-sandbox')
 options.add_argument('disable-gpu')
+options.add_argument('--proxy-server=%s' % PROXY)
 
 driver = webdriver.Chrome(chrome_options=options)
 
-driver.get('https://wikipedia.org')
+driver.get('https://www.whatismyip.com')
 driver.implicitly_wait(10)
 
-driver.get_screenshot_as_file('wiki.png')
+driver.get_screenshot_as_file('ip.png')
