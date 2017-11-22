2c90a51e151b17d9c16f6751a47c57b113f33c1d
rn4ir <rn@rahul-nair.com>
2017-11-21 21:08:07 -0500

    fonts and additional deps

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ VERSION=$(curl http://chromedriver.storage.googleapis.com/LATEST_RELEASE)
 DL_DIR="browsers/chromium-local/chrome-linux/"
 
 sudo yum install GConf2 libXScrnSaver -y
+sudo yum install ipa-gothic-fonts xorg-x11-fonts-100dpi xorg-x11-fonts-75dpi xorg-x11-utils xorg-x11-fonts-cyrillic xorg-x11-fonts-Type1 xorg-x11-fonts-misc -y
 
 wget "http://chromedriver.storage.googleapis.com/$VERSION/chromedriver_$PLATFORM.zip"
 sudo unzip chromedriver_$PLATFORM.zip -d $(echo $PATH | cut -d: -f1)
