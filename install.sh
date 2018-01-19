5a4eb204a70fa5d10abba7f75ae95d65fc8b3490
rn4ir <rn@rahul-nair.com>
2018-01-19 09:37:16 -0500

    install chromium and firefox globally

--- install.sh
+++ install.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-# check if the current user is root
-if [[ $(sudo whoami) != "root" ]];then
-    echo -e "Looks like the current user '$(whoami)' is either a 'non-root' user or is not a 'sudoer'.\nPlease switch to 'root' or grant this user 'sudo' privileges.\nExiting....\n"
+install_chromium (){
+    yum install epel-release -y
+    yum install chromium -y
+
+    echo -e "\nChromium installation complete.\n"
+    which chromium
+}
+
+install_firefox (){
+    cd /usr/local
+    wget https://ftp.mozilla.org/pub/firefox/releases/57.0.4/linux-x86_64/en-US/firefox-57.0.4.tar.bz2
+    tar xvjf firefox*
+    ln -s /usr/local/firefox/firefox /usr/bin/firefox
+
+    echo -e "\nFirefox installation complete.\n"
+    which firefox
+}
+
+
+if [[ $(/usr/bin/id -u) != "0" ]]; then
+    echo -e "This looks like a 'non-root' user.\nPlease switch to 'root', or run the script with 'sudo'."
     exit
 fi
+
+install_chromium
+install_firefox
