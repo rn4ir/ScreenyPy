f7ea7346802b0c2258235fc06e63a57aba972181
rn4ir <rn@rahul-nair.com>
2018-01-29 18:02:57 -0500

    new chromium+firefox functions

--- screeny.py
+++ screeny.py
@@ -15,47 +15,49 @@ PROXY_LIST = PROXY_LISTTXT.splitlines()
 
 ctr = 0
 
+def shots_chromium(){
+    for proxy in PROXY_LIST:
+        ctr = ctr+1
+        FILE_NAME = proxy.split(':')[0] + "_chromium" + str(ctr) + ".png"
+        options = webdriver.ChromeOptions()
 
+        options.binary_location = CHROME_BIN
+        options.add_argument('headless')
+        options.add_argument('window-size=1200x600')
+        options.add_argument('disable-gpu')
+        options.add_argument('--proxy-server=%s' % proxy)
 
-for proxy in PROXY_LIST:
-    ctr = ctr+1
-    """
-    FILE_NAME = proxy.split(':')[0] + "_chromium" + str(ctr) + ".png"
-    options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome(chrome_options=options)
 
-    options.binary_location = CHROME_BIN
-    options.add_argument('headless')
-    options.add_argument('window-size=1200x600')
-    options.add_argument('disable-gpu')
-    options.add_argument('--proxy-server=%s' % proxy)
+        start_time = time.time()
+        driver.get('http://en.wikipedia.org/')
+        print("--- screenshot " + proxy.split(':')[0] + "_chromium" + str(ctr) + ".png"  +  " generated in %s seconds ---" % (time.time() - start_time))
+        driver.implicitly_wait(10)
 
-    driver = webdriver.Chrome(chrome_options=options)
+        driver.get_screenshot_as_file(FILE_NAME)
+}
 
-    start_time = time.time()
-    driver.get('http://en.wikipedia.org/')
-    print("--- screenshot " + proxy.split(':')[0] + "_chromium" + str(ctr) + ".png"  +  " generated in %s seconds ---" % (time.time() - start_time))
-    driver.implicitly_wait(10)
+def shots_firefox(){
+    for proxy in PROXY_LIST:
+        ctr = ctr+1
+        FILE_NAME = proxy.split(':')[0] + "_firefox" + str(ctr) + ".png"
 
-    driver.get_screenshot_as_file(FILE_NAME)
-    """
-    FILE_NAME = proxy.split(':')[0] + "_firefox" + str(ctr) + ".png"
-#    options = webdriver.Firefox()
+        options = Options()
+        options.binary_location = FIREFOX_BIN
+        options.add_argument('headless')
+        options.add_argument('window-size=1200x600')
+        options.add_argument('disable-gpu')
+        options.add_argument('--proxy-server=%s' % proxy)
 
-    options = Options()
-    options.binary_location = FIREFOX_BIN
-    options.add_argument('headless')
-    options.add_argument('window-size=1200x600')
-    options.add_argument('disable-gpu')
-    options.add_argument('--proxy-server=%s' % proxy)
+        driver = webdriver.Firefox(firefox_options=options)
 
-    driver = webdriver.Firefox(firefox_options=options)
+        start_time = time.time()
+        driver.get('http://en.wikipedia.org/')
+        print("--- screenshot " + proxy.split(':')[0] + "_firefox" + str(ctr) + ".png"  +  " generated in %s seconds ---" % (time.time() - start_time))
+        driver.implicitly_wait(10)
 
-    start_time = time.time()
-    driver.get('http://en.wikipedia.org/')
-    print("--- screenshot " + proxy.split(':')[0] + "_firefox" + str(ctr) + ".png"  +  " generated in %s seconds ---" % (time.time() - start_time))
-    driver.implicitly_wait(10)
-
-    driver.get_screenshot_as_file(FILE_NAME)
+        driver.get_screenshot_as_file(FILE_NAME)
+}
 
 def main():
     """
@@ -63,18 +65,19 @@ def main():
     Checks command-line arguments and calls the relevant function.
     """
 
-    cli_argparser = argparse.ArgumentParser(description='pymyip - A python script that generates screenshots of a website from various random locations.')
+    cli_argparser = argparse.ArgumentParser(description='screenypy - A python script that generates screenshots of a website from various random locations.')
     cli_argparser.add_argument('-c', '--chrome', nargs='?', const=1, help="Generates screenshots using Chromium", required=False)
     cli_argparser.add_argument('-f', '--firefox', nargs='?', const=1, help="Generates screenshots using Mozilla Firefox", required=False)
 
     cli_args = cli_argparser.parse_args()
 
     if (cli_args.chrome):
-        (myip("a"))
+        shots_chromium()
     elif (cli_args.firefox):
-        (myip("l"))
+        shots_firefox()
     else:
-        print ("\nYour WAN IP is: " + myip() + "\n")
+        shots_chromium()
+        shots_firefox()
 
 if __name__ == '__main__':
     sys.exit(main())
